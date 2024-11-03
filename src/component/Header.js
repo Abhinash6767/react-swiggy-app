91c6536db2733e12ae32377b11bfcd46647b7404
Abhinash67 <abhinashpatnaik67@gmail.com>
2024-11-03 21:07:47 +0530

    routing functionality added

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [loginBtn, setLoginBtn] = useState('Login')
     return (
       <div className="header">
         <div className="logo-container">
@@ -11,10 +14,16 @@ const Header = () => {
         </div>
         <div className="nav-items">
           <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
+            <li>
+              <Link to='/'>Home</Link></li>
+            <li>
+              <Link to='/about'>About Us</Link></li>
+            <li>
+              <Link to='/contact'>Contact Us</Link></li>
             <li>Cart</li>
+            <button onClick={() => {
+              loginBtn === 'Login' ? setLoginBtn('Logout') : setLoginBtn('Login')
+            }}>{loginBtn}</button>
           </ul>
         </div>
       </div>
